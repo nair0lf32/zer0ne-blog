a0f42ba4cd5427367bc32f80bcdbdda03b756dde
EDEMESSI Florian <35136136+nair0lf32@users.noreply.github.com>
2022-02-14 02:30:25 +0000

    Add files via upload
    Former-commit-id: ef3cf1a172ac898035d5476666d05e2adbfdec3d

---
+++ THM/CTFs/Gallery/initialize.php
@@ -0,0 +1,11 @@
+'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+
+if(!defined('base_url')) define('base_url',"http://" . $_SERVER['SERVER_ADDR'] . "/gallery/");
+	if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+	if(!defined('dev_data')) define('dev_data',$dev_data);
+	if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+	if(!defined('DB_USERNAME')) define('DB_USERNAME',"gallery_user");
+	if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"passw0rd321");
+	if(!defined('DB_NAME')) define('DB_NAME',"gallery_db");
+	?>
+	
